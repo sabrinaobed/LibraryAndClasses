a8504b0b1c72d672d48cf5913b4ca1f83a971fea
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-24 11:19:38 +0100

    Add project files.

---
+++ LibraryAndClasses/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryAndClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
