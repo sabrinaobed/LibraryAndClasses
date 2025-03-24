72e96d378a3fc9486fbe67cb6fcc22f393565f63
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-24 11:30:01 +0100

    Did changes to datatypes of some properties

--- MyClassLibrary/Customer.cs
+++ MyClassLibrary/Customer.cs
@@ -7,8 +7,8 @@
         public string Name { get; set; }
         public string  Email { get; set; }
         public string?  Address { get; set; }
-        public string? PostNumber { get; set; }
+        public int? PostNumber { get; set; }
         public string? City { get; set; }
-        public string? IsVIP { get; set; }
+        public int? IsVIP { get; set; }
     }
 }
