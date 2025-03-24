efda23097d65fe3fb3b8de8347b0b483072619df
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-24 11:28:51 +0100

    Wrote all properties of customer class

--- MyClassLibrary/Customer.cs
+++ MyClassLibrary/Customer.cs
@@ -2,6 +2,13 @@
 {
     public class Customer
     {
-
+        //Properties
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string  Email { get; set; }
+        public string?  Address { get; set; }
+        public string? PostNumber { get; set; }
+        public string? City { get; set; }
+        public string? IsVIP { get; set; }
     }
 }
