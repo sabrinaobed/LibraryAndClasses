8941d85934594d29bad6bdc35a846800ecd2d652
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-03-24 11:46:57 +0100

    Created a new instance of List of 4 customers

--- LibraryAndClasses/Program.cs
+++ LibraryAndClasses/Program.cs
@@ -6,10 +6,22 @@ namespace LibraryAndClasses
     {
         static void Main(string[] args)
         {
-            //create an object 
-            var firstCustomer = new Customer(1, "Sabrina", "Sabr@gmail.com");
+            //create an object of a customer
+            var firstCustomer = new Customer(1, "Obed Samuel", "obed@gmail.com");
             Console.WriteLine(firstCustomer.Name);
+
+            //an instance with a list of 4 customers
+            var customers = new List<Customer>
+            {
+                new Customer(1, "Sabrina Obed", "sabrina@gmail.com"),
+                new Customer(2, "Verdha Yousaf", "verdha@gmail.com"),
+                new Customer(3, "Sadrick John", "sadrick@gmail.com"),
+                new Customer(4, "Eliza John", "eliza@gmail.com")
+            };
+
+
         }
+
     }
 }
 
